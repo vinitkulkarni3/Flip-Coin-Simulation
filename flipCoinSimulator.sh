06869663a778b6b23ef9a5a4804c3eca93a715a6
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-17 00:42:53 +0530

    Added:Showing head and tails through loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #Head or Tail Simulation
 
+head=0
+tail=0
+
+for (( i=0; i<10; i++ ))
+do
 random=$(($RANDOM%2+1))
 if [ $random == 1 ]
 then
         echo "heads"
+        ((head++))
 else
         echo "Tails"
+        ((tails++))
 fi
+done
