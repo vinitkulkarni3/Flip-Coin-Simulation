e779642c6b02a08c82a1c34d5c60cf0db030534d
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-17 00:45:37 +0530

    Added:Won or Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,16 +3,40 @@
 
 head=0
 tail=0
+n=1
+temp=21
 
-for (( i=0; i<10; i++ ))
+while [ $n != 0 ]
 do
-random=$(($RANDOM%2+1))
-if [ $random == 1 ]
-then
-        echo "heads"
-        ((head++))
-else
-        echo "Tails"
-        ((tails++))
-fi
+        random=$(($RANDOM%2+1))
+        if [ $random == 1 ]
+        then
+                echo "heads"
+                ((head++))
+                if [ $head -eq $temp ]
+                then
+                        echo "Head is won"$head "tail"$tails
+                        a=$(($head - $tails))
+                        echo "difference between head and tail is "$a
+                        exit
+                elif [ $head -eq $tails ]
+                then
+                        echo "-----TIE-----"
+                        exit
+                fi
+        else
+                echo "Tails"
+                ((tails++))
+                if [ $tails -eq $temp ]
+                then
+                        echo "TAILS is won"$tails" head"$head
+                        b=$(($tails-$head))
+                        echo "difference between tail and head is "$b
+                        exit
+                elif [ $tails -eq $head ]
+                then
+                        echo "-----TIE-----"
+                        exit
+                fi
+        fi
 done
